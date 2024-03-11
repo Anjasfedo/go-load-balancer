2604144be8e4ceed63356de8f51f8c4f9ff1cff4
AnjasFedo <fedoaf98@gmail.com>
2024-03-11 21:12:00 +0700

    Function to Create New Server

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http/httputil"
+	"net/url"
+	"os"
 )
 
 type simpleServer struct {
 	address string
 	proxy   *httputil.ReverseProxy
+}
+
+func newSimpleServer(address string) *simpleServer {
+	serverUrl, err := url.Parse(address)
+	handleErr(err)
+
+	return &simpleServer{
+		address: address,
+		proxy:   httputil.NewSingleHostReverseProxy(serverUrl),
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
 }
 
 func main() {
